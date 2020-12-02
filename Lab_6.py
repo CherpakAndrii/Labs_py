99768d2ecd237ddb743d54c88208668a259fcaeb
CherpakAndrii <71765448+CherpakAndrii@users.noreply.github.com>
2020-12-02 16:34:20 +0200

    Add files via upload

--- Lab_6.py
+++ Lab_6.py
@@ -1,21 +1,28 @@
 import math
-eps = 0.00001
-x1 = 0.818
-x2 = 1.403
-x3 = 3.141
-a = 'math.log(1+x**2)'
-b = 'math.e**(-1*x**2)'
-c = '2/x*math.cos(x/2)'
+eps, x1, x2, x3 = 0.00001, 0.818, 1.403, 3.141
 
 
-def get_area(x_min, x_max, f1, f2):
+def get_area_1(x_min, x_max):
     s = 0
     x = x_min
     while x<=x_max:
-        s += (eval(f1) - eval(f2)) * eps
+        a = math.log(1+x**2)
+        b = math.e**(-1*x**2)
+        s += (a-b) * eps
         x+=eps
     return s
 
 
-area = get_area(x1, x2, a, b) + get_area(x2, x3, c, b)
+def get_area_2(x_min, x_max):
+    s = 0
+    x = x_min
+    while x<=x_max:
+        c = 2/x*math.cos(x/2)
+        b = math.e**(-1*x**2)
+        s += (c - b) * eps
+        x+=eps
+    return s
+
+
+area = get_area_1(x1, x2) + get_area_2(x2, x3)
 print('The area of the figure limited by these functions: ', area)
